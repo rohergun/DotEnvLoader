44504a8df8d7c85cddd927fecae2e19d9d330125
RohatErgun <rohatergun06@gmail.com>
2026-04-24 15:06:37 +0200

    feat: add stub signatures for DotEnvLoader and EnvContext

---
+++ src/main/java/io/github/rohergun/dotenv/DotEnvLoader.java
@@ -0,0 +1,18 @@
+package io.github.rohergun.dotenv;
+
+import java.nio.file.Path;
+
+public class DotEnvLoader {
+
+    public static void load(){
+        // Todo
+    }
+
+    public static void load(String filepath){
+        // Todo
+    }
+
+    public static void load(Path path){
+        // Todo
+    }
+}
---
+++ src/main/java/io/github/rohergun/dotenv/EnvContext.java
@@ -0,0 +1,26 @@
+package io.github.rohergun.dotenv;
+
+import java.util.Map;
+
+public final class EnvContext {
+
+    // Writing to DotEnvLoader
+    static void put(String key, String value){
+        // Todo
+    }
+
+    static void putAll(Map<String, String> envEntries){
+        // Todo
+    }
+
+    // Reading from DotEnvLoader
+    public static String get(String key){
+        // Todo
+    }
+
+    // returns all values that loaded to .env
+    public static Map<String, String> all(){
+        // Todo
+    }
+
+}
