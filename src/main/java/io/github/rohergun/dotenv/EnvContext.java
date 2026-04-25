37853ff57919e4372151c78e00651589132c4740
RohatErgun <rohatergun06@gmail.com>
2026-04-25 18:17:31 +0200

    feat(EnvContext): add put method for single key:value entry

--- src/main/java/io/github/rohergun/dotenv/EnvContext.java
+++ src/main/java/io/github/rohergun/dotenv/EnvContext.java
@@ -1,12 +1,23 @@
 package io.github.rohergun.dotenv;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public final class EnvContext {
+    // storing .env values
+    private static final Map<String, String> store = new HashMap<>();
 
     // Writing to DotEnvLoader
     static void put(String key, String value){
-        // Todo
+        if (key == null){
+            throw new IllegalArgumentException("Key must be defined");
+        }
+        if (value == null){
+            throw new IllegalArgumentException("Value must be defined");
+        }
+        synchronized (store) {
+            store.put(key, value);
+        }
     }
 
     static void putAll(Map<String, String> envEntries){
