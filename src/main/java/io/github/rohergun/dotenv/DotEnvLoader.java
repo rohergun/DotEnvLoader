6184937b2b8613875ea9df17f45db4b40a93c074
RohatErgun <rohatergun06@gmail.com>
2026-04-27 11:02:41 +0200

    feat(DotEnvLoader): add parse method and update pom.xml

--- src/main/java/io/github/rohergun/dotenv/DotEnvLoader.java
+++ src/main/java/io/github/rohergun/dotenv/DotEnvLoader.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class DotEnvLoader {
@@ -29,6 +32,37 @@ public class DotEnvLoader {
 //        Todo Read lines from the input path
 //        Todo Parse the lines to source names
 //        Todo EnvContext.putAll(parsed);
+    }
+
+    static Map<String, String> parse(List<String> lines){
+        Map<String, String> parsedLines = new HashMap<>();
+
+        for (int i=0; i<lines.size(); i++){
+            String raw = lines.get(i);
+
+            if (raw.isBlank()) {
+                continue;
+            }
+
+            int equalsIndex = raw.indexOf('=');
+            if (equalsIndex < 0){
+                // Todo add custom exception
+                System.err.println("Missing '=' separator between key=value");
+            }
+
+            String key = raw.substring(0, equalsIndex);
+            String val = raw.substring(equalsIndex + 1);
+            if (key.isEmpty()){
+                // Todo add custom exception
+                System.err.println("Key is missing");
+            }
+            if (!KEY_PATTERN.matcher(key).matches()){
+                // Todo add custom exception
+                System.err.println("Key is not valid pattern");
+            }
+            parsedLines.put(key, val);
+        }
+        return Collections.unmodifiableMap(parsedLines);
     }
 
 
