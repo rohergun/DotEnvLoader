bdf4b7bfb786d60dbd665b90eb0f7bc6750b9e33
RohatErgun <rohatergun06@gmail.com>
2026-04-27 17:03:34 +0200

    feat(CustomException): add DotEnvParseException

---
+++ src/main/java/io/github/rohergun/dotenv/DotEnvParseException.java
@@ -0,0 +1,23 @@
+package io.github.rohergun.dotenv;
+
+public class DotEnvParseException extends RuntimeException{
+    private final String sourceFile;
+    private final int lineNumber;
+    private final String rawLine;
+
+    public DotEnvParseException(String sourceFile, int lineNumber,
+                                String rawLine, String reason){
+        super(buildMessage(sourceFile, lineNumber, rawLine, reason));
+        this.sourceFile = sourceFile;
+        this.lineNumber = lineNumber;
+        this.rawLine    = rawLine;
+    }
+
+    private static String buildMessage(String sourceFile, int lineNumber,
+                                       String rawLine, String reason){
+        return String.format("Parse error in '%s' at line %d: %s%n  offending line: \"%s\"",
+                sourceFile, lineNumber, reason, rawLine
+        );
+    }
+
+}
